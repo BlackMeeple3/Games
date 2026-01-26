94fb791853aec300537f9d43311415ab3b9c4afd
BlackMeeple3 <francescodisab@gmail.com>
2026-01-26 17:17:16 +0100

    Update script.js

--- script.js
+++ script.js
@@ -968,7 +968,7 @@ function createFloatingObjects() {
     // Posizione e animazione random
     el.style.left = Math.random() * 100 + '%';
     el.style.animationDuration = (18 + Math.random() * 12) + 's';
-    el.style.animationDelay = Math.random() * 10 + 's';
+    // el.style.animationDelay = Math.random() * 10 + 's';
 
     container.appendChild(el);
   }
