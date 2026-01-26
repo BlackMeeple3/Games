74f5495af1178191b8171d1f7f5ac9a8bf5e8ad2
BlackMeeple3 <francescodisab@gmail.com>
2026-01-26 16:52:42 +0100

    Add random positioning to elements in animation

--- script.js
+++ script.js
@@ -967,6 +967,8 @@ function createFloatingObjects() {
 
     // Posizione e animazione random
     el.style.left = Math.random() * 100 + '%';
+    el.style.top = Math.random() * 100 + '%';
+
     el.style.animationDuration = (18 + Math.random() * 12) + 's';
     el.style.animationDelay = Math.random() * 10 + 's';
 
